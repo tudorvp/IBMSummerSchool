ea44a74426c5f129717d5732ab8bbe6158588c0c
Tudor D <45359939+tudorvp@users.noreply.github.com>
2020-07-11 11:42:53 +0300

    Add files via upload

---
+++ Seminar 1.0/ProductionCode/Greeting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Seminar1.ProductionCode
+{
+    internal class Greeting
+    {
+        public Greeting()
+        {
+        }
+        internal string Greet(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello, my friend.";
+            }
+
+
+            if (name == name.ToUpper())
+            {
+                return $"HELLO, {name.ToUpper()}!";
+            }
+            return $"Hello, {name}.";
+        }
+
+        internal string Greet(string[] names)
+        {
+            string result = String.Empty;
+            if (names.Length == 2)
+            {
+                result = $"Hello, {names[0]} and {names[1]}.";
+            }
+            return result;
+        }
+    }
+}
---
+++ Seminar 1.0/Tests/GreetingTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using Seminar1.ProductionCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1.Tests
+{
+    [TestFixture]
+    class GreetingTest
+    {
+        Greeting greeting = new Greeting();
+
+        public void ExecuteTest(string name, string expected)
+        {
+            //Arrange
+
+            //Act
+            string result = greeting.Greet(name);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void ShouldGreetBob()
+        {
+            ExecuteTest("Bob","Hello, Bob.");
+        }
+        [Test]
+        public void ShouldHandleNull()
+        {
+
+            ExecuteTest(null, "Hello, my friend.");
+        }
+        [Test]
+        public void ShouldGreetGigi()
+        {
+
+            ExecuteTest("Gigi", "Hello, Gigi.");
+        }
+        [Test]
+        public void ShouldHandleShouting()
+        {
+
+            ExecuteTest("JERRY", "HELLO, JERRY!");
+        }
+
+        [Test]
+        public void ShouldHandleTwoNames()
+        {
+            //Arrange
+            string[] names = { "Jill", "Jane" };
+
+            //Act
+            string result = greeting.Greet(names);
+            //Assert
+            Assert.AreEqual("Hello, Jill and Jane.", result);
+
+        }
+    }
+}
